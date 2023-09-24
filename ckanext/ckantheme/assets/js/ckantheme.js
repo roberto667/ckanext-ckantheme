f10033692174ed5e947fe9fba522d189d329edff
roberto667 <112992674+roberto667@users.noreply.github.com>
2023-09-24 20:44:05 -0300

    update js

--- ckanext/ckantheme/assets/js/ckantheme.js
+++ ckanext/ckantheme/assets/js/ckantheme.js
@@ -1,1 +1,21 @@
 
+document.addEventListener("DOMContentLoaded", function () {
+    var dropdownButton = document.querySelector(".navbar-toggle");
+    var dropdownContent = document.getElementById("dropdown-content");
+
+    dropdownButton.addEventListener("click", function () {
+        if (dropdownContent.classList.contains("show")) {
+            dropdownContent.classList.remove("show");
+            dropdownButton.setAttribute("aria-expanded", "false");
+        } else {
+            dropdownContent.classList.add("show");
+            dropdownButton.setAttribute("aria-expanded", "true");
+        }
+    });
+});
+
+
+
+
+
+
