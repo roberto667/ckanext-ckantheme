3b440b0182f6619c03402ac8dd6d25bcc4669cc2
roberto667 <112992674+roberto667@users.noreply.github.com>
2023-09-24 21:05:31 -0300

    update js

--- ckanext/ckantheme/assets/js/ckantheme.js
+++ ckanext/ckantheme/assets/js/ckantheme.js
@@ -1,14 +1,1 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var dropdownButton = document.querySelector(".organization button");
-    var dropdownContent = document.getElementById("groupsdown");
 
-    dropdownButton.addEventListener("click", function () {
-        if (dropdownContent.classList.contains("show")) {
-            dropdownContent.classList.remove("show");
-            dropdownButton.setAttribute("aria-expanded", "false");
-        } else {
-            dropdownContent.classList.add("show");
-            dropdownButton.setAttribute("aria-expanded", "true");
-        }
-    });
-});
